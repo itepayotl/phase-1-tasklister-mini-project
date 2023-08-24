af974e973febc5c530b2512a6df4dc80e4a4bab0
Israel <itepayotl@yahoo.com>
2023-08-24 16:53:09 -0700

    display task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  addingEventListeners()
+})
+
+function addingEventListeners() {
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  console.log(e)
+  const task = e.target[0].value
+ 
+
+  displayTask(task)
+}
+function displayTask(task){
+   const taskUl = document.getElementById("tasks")
+  
+   const taskLi = document.createElement("li")
+   taskLi.textContent = task
+   console.log(taskLi)
+   taskUl.appendChild(taskLi)
+}
